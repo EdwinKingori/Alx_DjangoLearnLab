d0ce9a7dcf623f0c381fbaac89bcd5ae25b17eb1
Edwin King'ori <eddmunyiri@gmail.com>
2024-11-17 20:29:28 +0300

    modification

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -31,9 +31,9 @@ urlpatterns = [
     path('login', login, name='login'),
     path('logout', logout, name='logout'),
 
-    # path('admin-view/', views.admin_view, name='admin_view'),
-    # path('librarian-view/', views.librarian_view, name='librarian_view'),
-    # path('member-view/', views.member_view, name='member_view'),
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
 
     path("books/add/", views.add_book, name="add_book"),
     path("books/<int:book_id>/edit/", views.edit_book, name="edit_book"),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
 from . forms import BookForm
 from django.contrib.auth.decorators import permission_required
 from django.contrib import messages
@@ -134,3 +136,30 @@ def LibraryDetailView(request, pk):
         'library': library
     }
     return render(request, 'relationship_app/library_detail.html', context)
+
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
