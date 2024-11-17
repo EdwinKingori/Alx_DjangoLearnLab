62cae1dc8c450cf7ca0082384d0d95c5a8aa0230
Edwin King'ori <eddmunyiri@gmail.com>
2024-11-17 18:16:42 +0300

    models config

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
