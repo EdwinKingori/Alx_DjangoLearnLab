d519dcedbaba65010998fe78abd9640e41697d72
Edwin King'ori <eddmunyiri@gmail.com>
2024-11-17 20:24:37 +0300

    modification

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books
 from django.urls import path
 from .views import list_books, LibraryDetailView, register, login, logout, LoginView, LogoutView
+from .views import add_book, edit_book, delete_book
 
 from . import views
 
@@ -29,6 +30,22 @@ urlpatterns = [
     path('register', register, name='register'),
     path('login', login, name='login'),
     path('logout', logout, name='logout'),
+
+    # path('admin-view/', views.admin_view, name='admin_view'),
+    # path('librarian-view/', views.librarian_view, name='librarian_view'),
+    # path('member-view/', views.member_view, name='member_view'),
+
+    path("books/add/", views.add_book, name="add_book"),
+    path("books/<int:book_id>/edit/", views.edit_book, name="edit_book"),
+    path("books/<int:book_id>/delete/",
+         views.delete_book, name="delete_book"),
+
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
+
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
 
 
 ]
