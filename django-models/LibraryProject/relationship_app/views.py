b1fbfaa2a91396f90bdbdeb074bf813443319175
Edwin King'ori <eddmunyiri@gmail.com>
2024-11-17 19:23:38 +0300

    detailview import

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic.detail import DetailView
+from django.views.generic import ListView
 from .models import Library, Book
 # Create your views here.
 
