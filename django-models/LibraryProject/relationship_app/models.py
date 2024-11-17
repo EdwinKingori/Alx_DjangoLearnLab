92f5be05c9dee5c618c581ea1df316be8681c843
Edwin King'ori <eddmunyiri@gmail.com>
2024-11-17 20:19:46 +0300

    models modification

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -11,11 +11,16 @@ class Author(models.Model):
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
+    publication_year = models.IntegerField(blank=True, null=True)
 
-    def __str__(self):
-        return self.title
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
 
 class Library(models.Model):
