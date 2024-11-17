b0d7c9ebcd36507064886b577945baddab8481fa
Edwin King'ori <eddmunyiri@gmail.com>
2024-11-17 19:22:41 +0300

    Using view functions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 app_name = 'relationship_app'
 urlpatterns = [
-    path('', views.BookList.as_view(), name='list-books'),
+    # path('', views.BookList.as_view(), name='list-books'),
+    # path('librarydetail/<int:pk>/',
+    #      views.LibraryDetail.as_view(), name='library-detail'),
+    path('', views.bookList, name='list-books'),
     path('librarydetail/<int:pk>/',
-         views.LibraryDetail.as_view(), name='library-detail'),
+         views.libraryDetail, name='library-detail'),
 
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,11 +4,27 @@ from .models import Library, Book
 # Create your views here.
 
 
-class LibraryDetail(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
+# class LibraryDetail(DetailView):
+#     model = Library
+#     template_name = 'relationship_app/library_detail.html'
 
 
-class BookList(ListView):
-    model = Book
-    template_name = 'relationship_app/list_books.html'
+# class BookList(ListView):
+#     model = Book
+#     template_name = 'relationship_app/list_books.html'
+
+
+def bookList(request):
+    books = Book.objects.all()
+    context = {
+        'books': books
+    }
+    return render(request, 'relationship_app/list_books.html', context)
+
+
+def libraryDetail(request, pk):
+    library = Library.objects.get(id=pk)
+    context = {
+        'library': library
+    }
+    return render(request, 'relationship_app/library_detail.html', context)
