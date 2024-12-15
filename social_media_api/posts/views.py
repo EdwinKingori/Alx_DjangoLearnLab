b22d0e35a6a831010c360265db5a6eab90fdc82d
Edwin King'ori <eddmunyiri@gmail.com>
2024-12-15 23:25:54 +0300

    modifications

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -12,6 +12,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics
 
+Like.objects.get_or_create(user=request.user, post=post)
 
 class PostPagination(PageNumberPagination):
     page_size = 10
@@ -49,7 +50,7 @@ class FeedView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-generics.get_object_or_404(Post, pk=pk)
+
 
 
 class LikePostView(APIView):
@@ -62,9 +63,11 @@ class LikePostView(APIView):
         except Post.DoesNotExist:
             return Response({"error": "Post not found."}, status=status.HTTP_404_NOT_FOUND)
 
+        Like.objects.get_or_create(user=request.user, post=post)
+
         like, created = Like.objects.get_or_create(
             user=request.user, post=post)
-        if created:
+        if like or created:
             # Generate a notification
             Notification.objects.create(
                 recipient=post.author,  # Assuming Post model has an author field
